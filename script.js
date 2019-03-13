e10ef6390b6b603327747a8d0fbd3eb8e07a0bdd
Raoul Simionas <raulsimionas@gmail.com>
2019-03-13 11:18:31 +0200

    Added sounds

--- script.js
+++ script.js
@@ -3,17 +3,26 @@ document.addEventListener('DOMContentLoaded', () => {
 	document.querySelectorAll('.string')
 		.forEach( function(button) {
 			button.onclick = function() {
-				console.log('clicked');
 				var strings = document.querySelectorAll('.string');
+				var string_id = button.dataset.string;
+				var audios = document.querySelectorAll('audio');
+
 				for (let string of strings) {
 					if ( string != this ) {
 						string.classList.remove('active');
 					}
 				}
+
 				if ( this.classList.contains('active') ) {
 					this.classList.remove('active');
+					document.getElementById(string_id).currentTime = 0;
 				} else {
 					this.classList.add('active');
+					for (let audio of audios) {
+						audio.pause();
+						audio.currentTime = 0;
+					}
+					document.getElementById(string_id).play();
 				}
 			}
 		}
